db3ad8906ed6f6f5c2bd4ee13150566775571235
yashjawale <yash25.j@gmail.com>
2023-09-25 09:07:07 +0530

    Change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://acesdit.github.io',
+  base: '/snippets',
   integrations: [tailwind()]
 });
