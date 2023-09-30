d7d78d81f7001b112c6fed84d056d3a7cae9a9f5
yashjawale <yash25.j@gmail.com>
2023-09-30 20:58:11 +0530

    Add empty submissions folder

--- test.sh
+++ test.sh
@@ -1,28 +1,28 @@
 rm -f src/pages/submissions/*
-for file in ALGORITHMS/*; do
-if [ -f "$file" ]; then
-    filename=$(basename "$file")
-    filename_noext="${filename%.*}"
-    file_extension="${filename##*.}"
-    echo "Creating $filename.md from $file"
-    
-    # Add front matter to the MD file
-    echo "---" > "src/pages/submissions/$filename.md"
-    echo "extension: $file_extension" >> "src/pages/submissions/$filename.md" # Add the extension
-    
-    # Get the author's username who committed the file
-    author_username=$(git log -1 --format='%an' -- "$file")
-    echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
-    echo "category: Algorithms" >> "src/pages/submissions/$filename.md"
-    echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
-    echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
-    
-    echo "---" >> "src/pages/submissions/$filename.md"
+          for file in ALGORITHMS/*; do
+              if [ -f "$file" ]; then
+                  filename=$(basename "$file")
+                  filename_noext="${filename%.*}"
+                  file_extension="${filename##*.}"
+                  echo "Creating $filename.md from $file"
+                  
+                  # Add front matter to the MD file
+                  echo "---" > "src/pages/submissions/$filename.md"
+                  echo "extension: $file_extension" >> "src/pages/submissions/$filename.md" # Add the extension
+                  
+                  # Get the author's username who committed the file
+                  author_username=$(git log -1 --format='%an' -- "$file")
+                  echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
+                  echo "category: Algorithms" >> "src/pages/submissions/$filename.md"
+                  echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
+                  echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
+                  
+                  echo "---" >> "src/pages/submissions/$filename.md"
 
-    echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
-    
-    cat "$file" >> "src/pages/submissions/$filename.md"
+                  echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
+                  
+                  cat "$file" >> "src/pages/submissions/$filename.md"
 
-    echo "\`\`\`" >> "src/pages/submissions/$filename.md"
-fi
-done
+                  echo "\n\`\`\`" >> "src/pages/submissions/$filename.md"
+              fi
+          done
