32b806c0cd83daa8c73fd018bda2c5031ed8ae83
yashjawale <yash25.j@gmail.com>
2023-10-01 16:36:18 +0530

    Fix responsive issue

---
+++ src/pages/submissions/Insertion Sort.cpp.md
@@ -0,0 +1,42 @@
+---
+extension: cpp
+author: Omkar Vasekar
+category: Algorithms
+layout: '../../layouts/SubmissionLayout.astro'
+title: Insertion Sort
+---
+```cpp
+#include <iostream>
+using namespace std;
+void insertion_sort(int n, int arr[])
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] <= arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+        }
+    }
+}
+int main()
+{
+    int array[50], i, n;
+    cout << "Enter the no. of elements in array: ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    insertion_sort(n, array);
+    cout << "\nSorted Order is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
+}
+
+```
--- test.sh
+++ test.sh
@@ -1,3 +1,30 @@
+rm -f src/pages/submissions/*
+          for file in ALGORITHMS/*; do
+              if [ -f "$file" ]; then
+                  filename=$(basename "$file")
+                  filename_noext="${filename%.*}"
+                  file_extension="${filename##*.}"
+                  echo "Creating $filename.md from $file"
+                  
+                  # Add front matter to the MD file
+                  echo "---" > "src/pages/submissions/$filename.md"
+                  echo "extension: $file_extension" >> "src/pages/submissions/$filename.md" # Add the extension
+                  
+                  # Get the author's username who committed the file
+                  author_username=$(git log --reverse -1 --format='%an' -- "$file")
+                  echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
+                  echo "category: Algorithms" >> "src/pages/submissions/$filename.md"
+                  echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
+                  echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
+                  
+                  echo "---" >> "src/pages/submissions/$filename.md"
+                  echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
+                  
+                  cat "$file" >> "src/pages/submissions/$filename.md"
+                  echo "\n\`\`\`" >> "src/pages/submissions/$filename.md"
+              fi
+          done
+
 for file in DATA\ STRUCTURES/*; do
               if [ -f "$file" ]; then
                   filename=$(basename "$file")
@@ -11,18 +38,92 @@ for file in DATA\ STRUCTURES/*; do
                   
                   # Get the author's username who committed the file
                   author_username=$(git log --reverse -1 --format='%an' -- "$file")
-                  git log -- "$file"
                   echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
                   echo "category: Data Structures" >> "src/pages/submissions/$filename.md"
                   echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
                   echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
                   
                   echo "---" >> "src/pages/submissions/$filename.md"
-
                   echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
                   
                   cat "$file" >> "src/pages/submissions/$filename.md"
-
                   echo "\n\`\`\`" >> "src/pages/submissions/$filename.md"
               fi
           done
+
+for file in APPSCRIPT/*; do
+              if [ -f "$file" ]; then
+                  filename=$(basename "$file")
+                  filename_noext="${filename%.*}"
+                  file_extension="${filename##*.}"
+                  echo "Creating $filename.md from $file"
+                  
+                  # Add front matter to the MD file
+                  echo "---" > "src/pages/submissions/$filename.md"
+                  echo "extension: $file_extension" >> "src/pages/submissions/$filename.md" # Add the extension
+                  
+                  # Get the author's username who committed the file
+                  author_username=$(git log --reverse -1 --format='%an' -- "$file")
+                  echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
+                  echo "category: AppScript" >> "src/pages/submissions/$filename.md"
+                  echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
+                  echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
+                  
+                  echo "---" >> "src/pages/submissions/$filename.md"
+                  echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
+                  
+                  cat "$file" >> "src/pages/submissions/$filename.md"
+                  echo "\n\`\`\`" >> "src/pages/submissions/$filename.md"
+              fi
+          done
+
+for file in BASH/*; do
+              if [ -f "$file" ]; then
+                  filename=$(basename "$file")
+                  filename_noext="${filename%.*}"
+                  file_extension="${filename##*.}"
+                  echo "Creating $filename.md from $file"
+                  
+                  # Add front matter to the MD file
+                  echo "---" > "src/pages/submissions/$filename.md"
+                  echo "extension: $file_extension" >> "src/pages/submissions/$filename.md" # Add the extension
+                  
+                  # Get the author's username who committed the file
+                  author_username=$(git log --reverse -1 --format='%an' -- "$file")
+                  echo "author: $author_username" >> "src/pages/submissions/$filename.md" # Add the author
+                  echo "category: Bash" >> "src/pages/submissions/$filename.md"
+                  echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename.md"
+                  echo "title: $filename_noext" >> "src/pages/submissions/$filename.md"
+                  
+                  echo "---" >> "src/pages/submissions/$filename.md"
+                  echo "\`\`\`$file_extension" >> "src/pages/submissions/$filename.md"
+                  
+                  cat "$file" >> "src/pages/submissions/$filename.md"
+                  echo "\n\`\`\`" >> "src/pages/submissions/$filename.md"
+              fi
+          done
+
+for file in INTERVIEW\ QUESTIONS/*; do
+              if [ -f "$file" ]; then
+                  filename=$(basename "$file")
+                  filename_noext="${filename%.*}"
+                  file_extension="${filename##*.}"
+                  echo "Creating $filename_noext.md from $file"
+                  
+                  # Add front matter to the MD file
+                  echo "---" > "src/pages/submissions/$filename_noext.md"
+                  echo "extension: $file_extension" >> "src/pages/submissions/$filename_noext.md" # Add the extension
+                  
+                  # Get the author's username who committed the file
+                  author_username=$(git log --reverse -1 --format='%an' -- "$file")
+                  echo "author: $author_username" >> "src/pages/submissions/$filename_noext.md" # Add the author
+                  echo "category: Interview Questions" >> "src/pages/submissions/$filename_noext.md"
+                  echo "layout: '../../layouts/SubmissionLayout.astro'" >> "src/pages/submissions/$filename_noext.md"
+                  echo "title: $filename_noext" >> "src/pages/submissions/$filename_noext.md"
+                  
+                  echo "---" >> "src/pages/submissions/$filename_noext.md"
+                  
+                  cat "$file" >> "src/pages/submissions/$filename_noext.md"
+              fi
+          done
+
