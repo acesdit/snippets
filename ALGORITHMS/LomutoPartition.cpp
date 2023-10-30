627b49bd9c2e4c3cab6c29c0c0f789ac23952b1e
Anisha <anishashinde700@gmail.com>
2023-10-30 20:03:52 +0530

    Adding Lomuto Partition Algo.

---
+++ ALGORITHMS/LomutoPartition.cpp
@@ -0,0 +1,35 @@
+//Lomuto Partition
+// average time complexity = O(n)
+// auxiliary space complexity = O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+int ipartition(int arr[], int l, int h){
+    int pivot = arr[h];
+    int i = l-1;
+    for(int j=l; j<=h-1;j++){
+        if(arr[j]<pivot){
+            i++;
+                swap(arr[i],arr[j]);
+
+        }
+        
+    }
+    swap(arr[i+1],arr[h]);
+    return i+1;
+
+};
+int main() {
+	
+    int arr[]={10,80,30,90,40,50,70};
+	
+	int n=sizeof(arr)/sizeof(arr[0]);
+	
+	ipartition(arr,0,n-1);
+	
+	for(int x: arr)
+	    cout<<x<<" ";
+};
+
+
+
