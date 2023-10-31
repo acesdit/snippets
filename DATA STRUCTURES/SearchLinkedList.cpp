389f0a84a3dea288369e9b93c1c991adb34052f9
Anisha <anishashinde700@gmail.com>
2023-10-31 23:28:57 +0530

    Searching an element in Linked List

---
+++ DATA STRUCTURES/SearchLinkedList.cpp
@@ -0,0 +1,47 @@
+//Iterate through the linked list element wise, and, compare the current element with the element to be found
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
+}
+
+int search(Node * head, int x){
+    int pos=1;
+    Node *curr=head;
+    while(curr!=NULL){
+        if(curr->data==x)
+            return pos;
+        else{
+            pos++;
+            curr=curr->next;
+        }
+    }
+    return -1;
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	printlist(head);
+	cout<<"Position of element in Linked List: "<<search(head,20);
+	return 0;
+} 
