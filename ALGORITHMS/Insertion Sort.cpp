a7645edc4246629b6c60aab6faca351af61533b0
Vidsp21 <patilvidula21@gmail.com>
2024-10-23 22:13:11 +0530

    added comments

--- ALGORITHMS/Insertion Sort.cpp
+++ ALGORITHMS/Insertion Sort.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 void insertion_sort(int n, int arr[])
 {
-    for (int i = 1; i < n; i++)
+     // Loop through each element in the array starting from the second one
+    for (int i = 1; i < n; i++) 
     {
+        // Compare the current element with previous elements
         for (int j = i; j > 0; j--)
         {
+            // If the current element is less than or equal to the previous element
             if (arr[j] <= arr[j - 1])
             {
+                // Swap them to put the current element in its correct position
                 swap(arr[j], arr[j - 1]);
             }
         }
@@ -15,17 +19,21 @@ void insertion_sort(int n, int arr[])
 }
 int main()
 {
-    int array[50], i, n;
+    int array[50], i, n; // Declare an array to hold up to 50 integers
     cout << "Enter the no. of elements in array: ";
-    cin >> n;
+    cin >> n; // Read the number of elements from the user
+   
+    // Loop to input the elements into the array
     for (i = 0; i < n; i++)
     {
-        cin >> array[i];
+        // Read each element into the array
+        cin >> array[i]; 
     }
 
+    // Call the insertion sort function
     insertion_sort(n, array);
-    cout << "\nSorted Order is: " << endl;
-    for (int i = 0; i < n; i++)
+    cout << "\nSorted Order is: " << endl; // Print the sorted array
+    for (int i = 0; i < n; i++) // Print each sorted element
     {
         cout << array[i] << " ";
     }
