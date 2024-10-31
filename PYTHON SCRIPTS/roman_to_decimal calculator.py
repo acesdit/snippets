86d794f271d545200cc223d2fc3163992cd35616
ashhh17-sys <ashhhx17@gmail.com>
2024-10-31 16:02:21 +0530

    roman to decimal calculator

---
+++ PYTHON SCRIPTS/roman_to_decimal calculator.py
@@ -0,0 +1,28 @@
+tallies = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+    # specify more numerals if you wish
+}
+
+def RomanNumeralToDecimal(romanNumeral):
+    sum = 0
+    for i in range(len(romanNumeral) - 1):
+        left = romanNumeral[i]
+        right = romanNumeral[i + 1]
+        if tallies[left] < tallies[right]:
+            sum -= tallies[left]
+        else:
+            sum += tallies[left]
+    sum += tallies[romanNumeral[-1]]
+    return sum
+
+# Example usage
+if __name__ == "__main__":
+    roman_numeral = input("Enter a Roman numeral: ")
+    decimal_value = RomanNumeralToDecimal(roman_numeral)
+    print(f"The decimal value of {roman_numeral} is {decimal_value}.")
